0f46881198f312dd806758b94339f731a6cd9bac
hugosenari <hugosenari@gmail.com>
2022-03-23 22:52:29 -0300

    feat(nimpkgs): remove lock

--- v1_0_4/flake.nix
+++ v1_0_4/flake.nix
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-static_server-v1_0_4.flake = false;
-  inputs.src-static_server-v1_0_4.ref   = "refs/tags/v1.0.4";
-  inputs.src-static_server-v1_0_4.owner = "bung87";
-  inputs.src-static_server-v1_0_4.repo  = "nimhttpd";
-  inputs.src-static_server-v1_0_4.type  = "github";
+  inputs.src-nimhttpd-v1_0_4.flake = false;
+  inputs.src-nimhttpd-v1_0_4.ref   = "refs/tags/v1.0.4";
+  inputs.src-nimhttpd-v1_0_4.owner = "bung87";
+  inputs.src-nimhttpd-v1_0_4.repo  = "nimhttpd";
+  inputs.src-nimhttpd-v1_0_4.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-static_server-v1_0_4"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimhttpd-v1_0_4"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-static_server-v1_0_4";
+    src  = deps."src-nimhttpd-v1_0_4";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
--- v1_0_8/flake.nix
+++ v1_0_8/flake.nix
@@ -7,19 +7,19 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-static_server-v1_0_8.flake = false;
-  inputs.src-static_server-v1_0_8.ref   = "refs/tags/v1.0.8";
-  inputs.src-static_server-v1_0_8.owner = "bung87";
-  inputs.src-static_server-v1_0_8.repo  = "nimhttpd";
-  inputs.src-static_server-v1_0_8.type  = "github";
+  inputs.src-nimhttpd-v1_0_8.flake = false;
+  inputs.src-nimhttpd-v1_0_8.ref   = "refs/tags/v1.0.8";
+  inputs.src-nimhttpd-v1_0_8.owner = "bung87";
+  inputs.src-nimhttpd-v1_0_8.repo  = "nimhttpd";
+  inputs.src-nimhttpd-v1_0_8.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-static_server-v1_0_8"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimhttpd-v1_0_8"];
   in lib.mkRefOutput {
     inherit self nixpkgs ;
-    src  = deps."src-static_server-v1_0_8";
+    src  = deps."src-nimhttpd-v1_0_8";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   };
